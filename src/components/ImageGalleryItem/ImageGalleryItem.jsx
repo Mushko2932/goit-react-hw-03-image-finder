1e56fd9325cf5ddfaa850c55a9ed14d69b96c40e
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-05-05 22:43:41 +0300

    update ImageGallery

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -4,13 +4,13 @@ import { GalleryItem, GalleryImage } from './ImageGalleryItem.styled';
 export const ImageGalleryItem = ({ items, toggleModal }) => {  
   const{largeImageURL, webformatURL, tags} = items;
   return (
-      <GalleryItem
-        onClick={() => {
-          toggleModal(largeImageURL, tags);
-        }}
-      >
-        <GalleryImage src={webformatURL} alt={tags} />
-      </GalleryItem>
+    <GalleryItem
+      onClick={() => {
+        toggleModal(largeImageURL, tags);
+      }}
+    >
+      <GalleryImage src={webformatURL} alt={tags} />
+    </GalleryItem>
   );
 };
 
